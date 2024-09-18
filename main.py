1aa821742442fc427d4ff2d9b6cb304ef0bb7257
RoeserHe825 <RoeserHe825@northwested.org>
2024-09-18 14:06:47 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# Henry Roeser
+# Sep 18 2024
+# Python Simple-Calc
+
+# Input
+num1 = float(input("Please Enter a number: "))
+num2 = float(input("Please Enter Another a number: "))
+
+# Process
+total = num1 + num2
+
+# Output
+
+
+print(f'First number entered was: {num1}')
+print(f'Second number entered was: {num2}')
+print("The sum of the two numbers is", str(total) + ".")
